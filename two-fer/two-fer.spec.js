9630e46a92506ffe3bd7c88245c704db020fdb55
johann gomez <johanngomez02@gmail.com>
2021-05-22 18:00:57 -0500

    first commit

---
+++ two-fer/two-fer.spec.js
@@ -0,0 +1,19 @@
+import { twoFer } from './two-fer';
+
+describe('twoFer()', () => {
+  test('no name given', () => {
+    expect(twoFer()).toEqual('One for you, one for me.');
+  });
+
+  test('a name given', () => {
+    expect(twoFer('Alice')).toEqual('One for Alice, one for me.');
+  });
+
+  test('another name given', () => {
+    expect(twoFer('Bob')).toEqual('One for Bob, one for me.');
+  });
+
+  test('Johann name given', () => {
+    expect(twoFer('Johann')).toEqual('One for Johann, one for me.');
+  });
+});
